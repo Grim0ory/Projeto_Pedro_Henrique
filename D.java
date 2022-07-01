8e3a860d502a0080c6825278a318752f63c72188
Pedro Henrique <pedrobox10@live.com>
2022-06-30 22:57:37 -0300

    Criação da classe D

---
+++ D.java
@@ -0,0 +1,19 @@
+//Pedro Henrique dos Santos Mateus
+
+public class D{
+	private String D1;
+	private String D2;
+	
+	public D(){
+		D1 = "Pedro";
+		D2 = "Ciência da Computação";
+	}
+	
+	public void MD1(){
+		System.out.println("Aprendendo");
+	}
+	
+	public void MD2(){
+		System.out.println("git e github");
+	}
+}
