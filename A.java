9e85e53222b1d1c7c61f1b9de190333364d1c89e
Pedro Henrique <pedrobox10@live.com>
2022-06-30 21:17:20 -0300

    Upload dos Arquivos

---
+++ A.java
@@ -0,0 +1,30 @@
+//Pedro Henrique dos Santos Mateus
+
+public class A{
+	private int A1;
+	private float A2;
+	
+	public A(){
+		A1 = 0;
+		A2 = 0;
+	}
+	public int getA1(){
+		return A1;
+	}
+	public float getA2(){
+		return A2;
+	}
+	public void setA1(int valor){
+		A1 = valor;
+	}
+	public void setA2(float valor){
+		A2 = valor;
+	}
+	public void MA1(){
+		cout << "MA1" << endl;
+	}
+	public void MA2(){
+		cout << "MA2" << endl;
+	}
+	
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B{
+	private int B1;
+	private float B2;
+	
+	public B(){
+		B1 = 0;
+		B2 = 0;
+	}
+	
+	public int getB1(){
+		return B1;
+	}
+	public float getB2(){
+		return B2;
+	}
+	public void setB1(int valor){
+		B1 = valor;
+	}
+	public void setB2(float valor){
+		B2 = valor;
+	}
+	public void MB1(){
+		cout << "MB1" << endl;
+	}
+	public void MB2(){
+		cout << "MB2" << endl;
+	}
+}
